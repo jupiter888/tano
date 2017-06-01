851e72096c9fe45cdeb1adecea931999ee731c62
Daniel Tracy <visual.eyes.success@gmail.com>
2017-06-01 13:29:09 -0700

    adding index.js to server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+var express= require('express');
+var app= express();
+var dirSite= __dirname + "../site";
+
+
+
+app.set('port', process.env.PORT ||3000);
+
+
+// 404 handler
+app.use(function(req,res) {
+    res.type('text/html'); 
+    res.status(404);
+    res.sendFile(dirSite + '/404.html'); 
+});
+
+
+// 500 handler
+app.use(function(req,res) {
+    console.error(err.stack);
+    res.type('text/html'); 
+    res.status(505);
+    res.sendFile(dirSite + '/500.html'); 
+});
+
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Server is up');
+});
